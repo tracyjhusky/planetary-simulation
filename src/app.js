ac285d0ab45b47ee3682163772246834b6ab3ee7
James Tracy <tracy.j@husky.neu.edu>
2017-02-22 17:20:25 -0500

    Creating and rendering planets.

--- src/app.js
+++ src/app.js
@@ -9,18 +9,21 @@ const STARS = 1000;
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d')
 const stars = [];
+const planets = [];
 
 //-------------Vars-------------//
 let paused;
+
 
 //-------------Control-------------//
 function init() {
   paused = false;
 
-  generateStars();
-
   canvas.width = WIDTH;
   canvas.height = HEIGHT;
+
+  generateStars();
+  generatePlanets();
 
   tick();
   setInterval(tick, 100);
@@ -39,6 +42,7 @@ function update() {
 
 function render() {
   renderBG();
+  renderPlanets();
 }
 
 init();
@@ -55,10 +59,36 @@ function generateStars() {
   }
 }
 
-function Planet() {
-  return {
+function generatePlanets() {
+  const planet = new Planet(
+    { x: 400, y: 400 },
+    { x: 0, y: 0 },
+    100,
+    '#cc6666');
+  planets.push(planet);
+}
 
+function Planet(pos, vel, mass, color) {
+  return {
+    pos,
+    vel,
+    mass,
+    color,
+    radius: Math.cbrt(mass * 3/(4 * Math.PI)),
+    distanceTo: function(other) {
+      const xDel = this.pos.x - other.pos.x;
+      const yDel = this.pos.y - other.pos.y;
+      return Math.sqrt(sq(xDel) + sq(yDel));
+    },
+    collidesWith: function(other) {
+      return this.size + other.size < this.distanceTo(other);
+    },
   }
+}
+
+//-------------Math-------------//
+function sq(n) {
+  return n * n;
 }
 
 //-------------View-------------//
@@ -73,5 +103,15 @@ function renderStars() {
     const star = stars[i];
     ctx.fillStyle = star.color;
     ctx.fillRect(star.xPos, star.yPos, 1, 1);
+  }
+}
+
+function renderPlanets() {
+  for(let i = 0; i < planets.length; i++) {
+    const planet = planets[i];
+    ctx.beginPath();
+    ctx.arc(planet.pos.x, planet.pos.y, planet.radius, 0, Math.PI * 2);
+    ctx.fillStyle = planet.color;
+    ctx.fill();
   }
 }
