67f60f9e20cbbf07b3631af8edb1b588d192f627
James Tracy <tracy.j@husky.neu.edu>
2017-02-22 15:40:32 -0500

    Boilerplate & added starfield generation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,77 @@
 'use-strict'
 
+//-------------Constants-------------//
+const WIDTH = 800;
+const HEIGHT = 450;
+const STARS = 1000;
+
+//-------------Refs-------------//
 const canvas = document.getElementById('canvas');
-console.log('running');
-//canvas.width = 200;
-//canvas.height = 200;
+const ctx = canvas.getContext('2d')
+const stars = [];
+
+//-------------Vars-------------//
+let paused;
+
+//-------------Control-------------//
+function init() {
+  paused = false;
+
+  generateStars();
+
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+
+  tick();
+  setInterval(tick, 100);
+}
+
+function tick() {
+  if(!paused) {
+    update();
+  }
+  render();
+}
+
+function update() {
+  // Do nothing.
+}
+
+function render() {
+  renderBG();
+}
+
+init();
+
+//-------------Model-------------//
+function generateStars() {
+  for(let i = 0; i < STARS; i++) {
+    const star = {
+      xPos: Math.floor(Math.random() * WIDTH),
+      yPos: Math.floor(Math.random() * HEIGHT),
+      color: 'hsl(250, 50%,' + Math.random() * 100 + '%)',
+    }
+    stars.push(star);
+  }
+}
+
+function Planet() {
+  return {
+
+  }
+}
+
+//-------------View-------------//
+function renderBG() {
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  renderStars();
+}
+
+function renderStars() {
+  for(let i = 0; i < stars.length; i++) {
+    const star = stars[i];
+    ctx.fillStyle = star.color;
+    ctx.fillRect(star.xPos, star.yPos, 1, 1);
+  }
+}
