58e410448be351875d84b5adf04647f609fd1346
James Tracy <tracy.j@husky.neu.edu>
2017-02-23 14:04:56 -0500

    Added gravity and planetary motion; Changed main.html to index.html

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@
 const WIDTH = 800;
 const HEIGHT = 450;
 const STARS = 1000;
+const PLANETS = 300;
+const G = 1;
 
 //-------------Refs-------------//
 const canvas = document.getElementById('canvas');
@@ -26,7 +28,7 @@ function init() {
   generatePlanets();
 
   tick();
-  setInterval(tick, 100);
+  setInterval(tick, 10);
 }
 
 function tick() {
@@ -37,7 +39,8 @@ function tick() {
 }
 
 function update() {
-  // Do nothing.
+  applyGravity();
+  updatePlanets();
 }
 
 function render() {
@@ -60,18 +63,21 @@ function generateStars() {
 }
 
 function generatePlanets() {
-  const planet = new Planet(
-    { x: 400, y: 400 },
-    { x: 0, y: 0 },
-    100,
-    '#cc6666');
-  planets.push(planet);
+  for(let i = 0; i < PLANETS; i++) {
+    const planet = new Planet(
+      { x: Math.random() * WIDTH, y: Math.random() * HEIGHT },
+      { x: 0, y: 0 },
+      Math.random() * 100 + 20,
+      '#ffffff');
+    planets.push(planet);
+  }
 }
 
 function Planet(pos, vel, mass, color) {
   return {
     pos,
     vel,
+    force: { x: 0, y: 0 },
     mass,
     color,
     radius: Math.cbrt(mass * 3/(4 * Math.PI)),
@@ -81,10 +87,65 @@ function Planet(pos, vel, mass, color) {
       return Math.sqrt(sq(xDel) + sq(yDel));
     },
     collidesWith: function(other) {
-      return this.size + other.size < this.distanceTo(other);
+      return this.distanceTo(other) < this.radius + other.radius;
     },
+  };
+}
+
+function applyGravity() {
+  for(let i = 0; i < planets.length; i++) {
+    const planet = planets[i];
+    for(let j = 0; j < planets.length; j++) {
+      if(i !== j) {
+        const other = planets[j];
+        const dist = planet.distanceTo(other);
+        const ydist = other.pos.y - planet.pos.y;
+        const xdist = other.pos.x - planet.pos.x;
+        const force = G * planet.mass * other.mass / sq(dist);
+        const xforce = (xdist / dist) * force;
+        const yforce = (ydist / dist) * force;
+        planet.force.x += xforce;
+        planet.force.y += yforce
+      }
+    }
   }
 }
+
+function updatePlanets() {
+  for(let i = 0; i < planets.length; i++) {
+    const planet = planets[i];
+    planet.vel.x += planet.force.x / planet.mass;
+    planet.vel.y += planet.force.y / planet.mass;
+    planet.pos.x += planet.vel.x;
+    planet.pos.y += planet.vel.y;
+    planet.force.x = 0;
+    planet.force.y = 0;
+    const j = collidesWithAny(planet)
+    if(j >= 0) {
+      impact(i, j);
+    }
+  }
+}
+
+function impact(i, j) {
+  const p1 = planets[i];
+  const p2 = planets[j];
+
+  planets.splice(i, 1);
+}
+
+function collidesWithAny(planet) {
+  for(let i = 0; i < planets.length; i++) {
+    const other = planets[i];
+    if(planet !== other) {
+      if(planet.collidesWith(other)) {
+        return i;
+      }
+    }
+  }
+  return -1;
+}
+
 
 //-------------Math-------------//
 function sq(n) {
